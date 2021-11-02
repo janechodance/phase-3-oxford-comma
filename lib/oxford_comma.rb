7663e7ee622e7042198a5e0a0f5dda11ecee0812
Jane <janechodance@gmail.com>
2021-11-02 00:28:37 -0400

    commit tree 4738b1b91912e4396da6c991e0629b56fd53f766

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    length = array.length
+    if length <= 1
+ array.join
+    elsif length == 2
+        last_word =array[-1]
+        first = array[0]
+        "#{first} and #{last_word}"
+    else
+        last_word= array[-1]
+        array.pop
+        array.join(", ") << ", and #{last_word}"
+        
+    end
 end
